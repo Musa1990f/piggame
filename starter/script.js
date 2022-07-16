9a3b3f91035cc5321cc558c0b27396a4061a124e
Lukenge Faridu Musa <95524786+Musa1990f@users.noreply.github.com>
2022-07-16 21:16:17 +0900

    switched players

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score');
 const current0EL = document.getElementById('current--0');
@@ -15,8 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-
+const score = [0,0]
 let currentScore = 1;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function (){
@@ -32,9 +35,16 @@ btnRoll.addEventListener('click', function (){
   if (dice !== 1 ){
 // add dice to current score
 currentScore += dice;
-current1EL.textContent =currentScore; // change later
-  }else {
 
+document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+// current1EL.textContent =currentScore; // change later
+  }else {
+// switch to next player
+document.getElementById(`current--${activePlayer}`).textContent = 0;
+activePlayer = activePlayer === 0 ? 1 : 0;
+currentScore = 0;
+player0El.classList.toggle('player--active');
+player1El.classList.toggle('player--active');
   }
-})
+});
 
